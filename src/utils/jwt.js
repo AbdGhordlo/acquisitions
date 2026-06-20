e4500a9c993566a2f1f704df9a05df81958ef4e1
AbdGhordlo <abdallahghordlo@gmail.com>
2026-06-20 16:28:43 +0300

    update jwt util and add cookies util

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,5 +1,5 @@
 import logger from '#config/logger.js';
-import jwt from 'jsonwebtoken';
+import jwt, { verify } from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'secret-key-change-in-production';
 const JWT_EXPIRES_IN = '1d';
@@ -7,9 +7,17 @@ const JWT_EXPIRES_IN = '1d';
 export const jwttoken = {
     sign: (payload) => {
         try {
-            
+            return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN});
         } catch (e) {
             logger.error('Failed to authenticate token', error);
+            throw new Error('Failed to authenticate token');
+        }
+    }
+    verify: (token) => {
+        try {
+            return jwt.verify(token, JWT_SECRET);
+        } catch (e) {
+            logger.error('Failed to authenticate token', e);
             throw new Error('Failed to authenticate token');
         }
     }
