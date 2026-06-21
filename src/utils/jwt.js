3e28b83a4830892d5605e246a822e8322a4e8f11
AbdGhordlo <abdallahghordlo@gmail.com>
2026-06-21 15:25:00 +0300

    add format util and update package.json

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,24 +1,24 @@
 import logger from '#config/logger.js';
-import jwt, { verify } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'secret-key-change-in-production';
 const JWT_EXPIRES_IN = '1d';
 
 export const jwttoken = {
-    sign: (payload) => {
-        try {
-            return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN});
-        } catch (e) {
-            logger.error('Failed to authenticate token', error);
-            throw new Error('Failed to authenticate token');
-        }
+  sign: payload => {
+    try {
+      return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+    } catch (e) {
+      logger.error('Failed to authenticate token', error);
+      throw new Error('Failed to authenticate token');
     }
-    verify: (token) => {
-        try {
-            return jwt.verify(token, JWT_SECRET);
-        } catch (e) {
-            logger.error('Failed to authenticate token', e);
-            throw new Error('Failed to authenticate token');
-        }
+  },
+  verify: token => {
+    try {
+      return jwt.verify(token, JWT_SECRET);
+    } catch (e) {
+      logger.error('Failed to authenticate token', e);
+      throw new Error('Failed to authenticate token');
     }
-}
+  },
+};
